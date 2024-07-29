5b7b158b045717a629a1ace11a569c87d899d76a
SanketTops <sanket.tops@gmail.com>
2024-07-29 15:14:04 +0530

    String

---
+++ Assignments/Module-1/pass.py
@@ -0,0 +1,9 @@
+"""for i in range(1,11):
+    pass
+    """
+
+a=32
+b=65
+
+if a<b:
+    pass
---
+++ Assignments/Module-1/string.py
@@ -0,0 +1,29 @@
+mystr="this is string"
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4]) #range - String Sliceing
+
+print(len(mystr))"""
+
+# --------------------------------------------------- #
+
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('i','Y'))
+#print(mystr.replace('This','That'))
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count('This'))
+#print(mystr.startswith('This'))
+#print(mystr.endswith('!'))
+#print(mystr.find('S'))
+#print(mystr.index('S'))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+print(mystr.title())
