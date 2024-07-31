3eebece4092c5d91bc79aeed44b9f4211f6b6ebe
SanketTops <sanket.tops@gmail.com>
2024-07-31 15:21:51 +0530

    List

---
+++ Assignments/Module-2_Collection/list_dynamic.py
@@ -0,0 +1,9 @@
+stdata=[]
+
+n=int(input("Enter number of Students:"))
+
+for i in range(n):
+    x=input("Enter your name:")
+    stdata.append(x)
+
+print(stdata)
---
+++ Assignments/Module-2_Collection/list_example.py
@@ -0,0 +1,51 @@
+data=['HTML','Python','JAVA','PHP','Angular','React']
+"""print(data)
+print(data[0])
+print(data[-1])
+print(data[0:3])
+print(data[:4])
+print(data[2:])"""
+
+print(len(data))
+
+"""if 'PHP' in data:
+    print("Yes...")
+else:
+    print("Noo")"""
+
+"""print(data)
+data[2]='Android'
+print(data)"""
+
+# ------------------------------------------ #
+
+"""print(data)
+
+for i in data:
+    print(i)"""
+
+# ------------------------------------------ #
+
+print(data)
+#data.append('CSS')
+#data.insert(2,'JS')
+#data.remove('Angular')
+#data.pop()
+#data.pop(1)
+#data.clear()
+#data.sort()
+#data.reverse()
+#del data[0]
+#del data
+print(data)
+
+
+#newdata=data.copy()
+#print(newdata)
+
+"""newdata=['C','C++','DS']
+print(newdata)
+
+#print(data+newdata)
+data.extend(newdata)
+print(data)"""
